7c500ce88e7b6689b558f8b45f5609aa39245a0e
Falcon Hawk <18nc021@ms.dendai.ac.jp>
2019-03-12 01:07:56 +0900

    initial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ""
+    };
+    this.handleType = this.handleType.bind(this);
+  }
+
+  parse() {
+    let output = this.state.input;
+    output = output.replace(/[\*\_]{2}([^\*\_]+)[\*\_]{2}/g, "<b>$1</b>");
+    output = output.replace(/[\#]{1}(.+)/g, "<h1>$1</h1>");
+    output = output.replace(
+      /[\[]{1}([^\]]+)[\]]{1}[\(]{1}([^\)\"]+)(\"(.+)\")?[\)]{1}/g,
+      '<a href="$2" title="$4">$1</a>'
+    );
+    output = output.replace(/\\n/g, "<br/>");
+    return output;
+  }
+
+  handleType(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <h1> Markdown 解析サイト</h1>
+      <div>
+        <div className="container">
+          <textarea placeholder="Input Text" onChange={this.handleType} />
+          <div className="content">{this.parse()}</div>
+          <div className="content">
+            <Parse input={this.state.input} />
+          </div>
+        </div>
+        <Footer />
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+function Parse(props) {
+  let output = props.input;
+  output = output.replace(/[\*\_]{2}([^\*\_]+)[\*\_]{2}/g, "<b>$1</b>");
+  output = output.replace(/[\#]{1}(.+)/g, "<h1>$1</h1>");
+  output = output.replace(
+    /[\[]{1}([^\]]+)[\]]{1}[\(]{1}([^\)\"]+)(\"(.+)\")?[\)]{1}/g,
+    '<a href="$2" title="$4">$1</a>'
+  );
+  output = output.replace(/\\n/g, "<br/>");
+  return <div>{<div dangerouslySetInnerHTML={{ __html: output }} />}</div>;
+}
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Other Projects</p>
+      <div className="button">
+        <a href="http://sujank.me/reactmovie">
+          <button>映画検索</button>
+        </a>
+        <a href="http://sujank.me/reactgame/">
+          <button>三目並べ</button>
+        </a>
+      </div>
+    </div>
+  );
+};
